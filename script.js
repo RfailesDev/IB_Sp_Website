634695bc65eacb41aa23077ddf0c0bd4953b2114
RfailesDev <86327915+RfailesDev@users.noreply.github.com>
2023-06-12 03:26:19 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,103 +1,71 @@
-const wheel = document.getElementById("wheel");
-const spinBtn = document.getElementById("spin-btn");
-const finalValue = document.getElementById("final-value");
-//Object that stores values of minimum and maximum angle for a value
-const rotationValues = [
-  { minDegree: 0, maxDegree: 180, value: 1 }, //2
-  { minDegree: 182, maxDegree: 360, value: 2 }, //6
-];
-//Size of each piece
-const data = [25, 71];
-//background color for each piece
-var pieColors = [
-  "#8b35bc",
-  "#ababab",
-];
-//Create chart
-let myChart = new Chart(wheel, {
-  //Plugin for displaying text on pie chart
-  plugins: [ChartDataLabels],
-  //Chart Type Pie
-  type: "pie",
-  data: {
-    //Labels(values which are to be displayed on chart)
-    labels: ["Storm Lonedoom", ""],
-    //Settings for dataset/pie
-    datasets: [
-      {
-        backgroundColor: pieColors,
-        data: data,
-      },
-    ],
-  },
-  options: {
-    //Responsive chart
-    responsive: true,
-    animation: { duration: 0 },
-    plugins: {
-      //hide tooltip and legend
-      tooltip: false,
-      legend: {
-        display: false,
-      },
-      //display labels inside pie chart
-      datalabels: {
-        color: "#ffffff",
-        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
-        font: { size: 24 },
-      },
-    },
-  },
-});
-//display value based on the randomAngle
-const valueGenerator = (angleValue) => {
-  for (let i of rotationValues) {
-    //if the angleValue is between min and max then display it
-    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      if (i.value == 1) {
-        Telegram.WebApp.ready();
-        Telegram.WebApp.sendData("1");
-      }
-      else {
-        Telegram.WebApp.ready();
-        Telegram.WebApp.sendData("2");
-      }
-      spinBtn.disabled = false;
-      break;
-    }
-  }
-};
+function showMessage(message) {
+  var messageContainer = document.getElementById("messageContainer");
+  messageContainer.innerHTML = `<div class="message">${message}</div>`;
+  messageContainer.style.display = "block";
 
-//Spinner count
-let count = 0;
-//100 rotations for animation and last rotation for result
-let resultValue = 101;
-//Start spinning
-spinBtn.addEventListener("click", () => {
-  spinBtn.disabled = true;
-  //Empty final value
-  finalValue.innerHTML = `<p>Good Luck!</p>`;
-  //Generate random degrees to stop at
-  let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
-  //Interval for rotation animation
-  let rotationInterval = window.setInterval(() => {
-    //Set rotation for piechart
-    /*
-    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
-    */
-    myChart.options.rotation = myChart.options.rotation + resultValue;
-    //Update chart with new value;
-    myChart.update();
-    //If rotation>360 reset it back to 0
-    if (myChart.options.rotation >= 360) {
-      count += 1;
-      resultValue -= 5;
-      myChart.options.rotation = 0;
-    } else if (count > 15 && myChart.options.rotation == randomDegree) {
-      valueGenerator(randomDegree);
-      clearInterval(rotationInterval);
-      count = 0;
-      resultValue = 101;
-    }
-  }, 10);
-});
+  setTimeout(function() {
+    messageContainer.style.display = "none";
+  }, 3000);
+}
+
+function checkLuckyDegrees(degrees) {
+  // Приводим градусы к диапазону от -360 до 360
+  degrees = degrees % 360;
+
+  // Нормализуем градусы в диапазоне от -180 до 180
+  if (degrees > 180) {
+    degrees -= 360;
+  } else if (degrees < -180) {
+    degrees += 360;
+  }
+
+  // Проверяем, находятся ли нормализованные градусы в счастливом диапазоне
+  if (degrees >= -13 && degrees <= 13) {
+    return true;//"Счастливые координаты!";
+  } else {
+    return false;//"Несчастливые координаты.";
+  }
+}
+
+function startRotation() {
+    rotateButton.disabled = true;
+    var image = document.getElementById("rotateImage");
+    image.style.transform = "rotate(0deg)";
+
+    let deg = Math.floor(Math.random()*360)+360*15 //5759
+
+
+    let rot = "rotate("+( deg )+"deg)"; //+360*15
+
+
+    setTimeout(function() {
+        var IsWin = checkLuckyDegrees(deg)
+        if (IsWin) {
+            showMessage("You WIN!");
+        }
+        else {
+            showMessage("You LOOSE!");
+        }
+        setTimeout(function() {
+            if (IsWin) {
+                Telegram.WebApp.ready();
+                Telegram.WebApp.sendData("1");
+            }
+            else {
+                Telegram.WebApp.ready();
+                Telegram.WebApp.sendData("2");
+            }
+        }, 1000);
+    }, 7000);
+
+
+    image.style.transform = rot;
+
+    //setTimeout(function() {
+    //  image.style.transform = "rotate(180deg)";
+
+    //  setTimeout(function() {
+    //    image.style.transform = "rotate(360deg)";
+    //  }, 1000);
+    //}, 1000);
+}
